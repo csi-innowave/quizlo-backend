4d9b695f4392e341a1cd4b4e4a447233af8d1e12
Yash <yg232004@gmail.com>
2024-04-13 18:07:57 +0530

    result routes and controllers

--- controllers/controller.js
+++ controllers/controller.js
@@ -6,8 +6,24 @@ async function getQuestions(req, res) {
 async function insertQuestions(req, res) {
   res.json("insert questions");
 }
+async function dropQuestions(req, res) {
+  res.json("Dropped all qs");
+}
+async function getResult(req, res) {
+  res.json("result api got rep ");
+}
+async function storeResult(req, res) {
+  res.json("result api post req");
+}
+async function deleteResult(req, res) {
+  res.json("result got deleted req");
+}
 
 module.exports = {
   getQuestions,
   insertQuestions,
+  dropQuestions,
+  getResult,
+  storeResult,
+  deleteResult,
 };
